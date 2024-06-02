7395ee4ff863d6fc708b78544a752c82c2b6c072
James Qian <jamesbarbk@gmail.com>
2024-06-02 11:41:12 -0700

    updated evaluations and added env_util

--- OtherModels/env.py
+++ OtherModels/env.py
@@ -7,66 +7,13 @@ from torch import Tensor
 ObsType = TypeVar("ObsType")
 ActType = TypeVar("ActType")
 
-class Env:
-    states : Tensor
-    actions : Tensor
-    rewards : Tensor
-    dones : Tensor
-    config_dict : Dict[str, Any]
-    dynamics_nn: nn.Module
-    rewards_nn: nn.Module
-    dones_nn: nn.Module
-
-    def __init__(self, chkpt_path: str = None):
-        self.load_from_chkpt(chkpt_path)
-
-    def load_from_chkpt(self, chk_path: str) -> None:
-        checkpoint = torch.load(chk_path)
-        self.config_dict = checkpoint['config']
-        self.dynamics = Dynamics(self.config_dict['state_dim'], 
-                                 self.config_dict['action_dim'], 
-                                 self.config_dict['hidden_dim'], 
-                                 self.config_dict['sequence_num'])
-        self.rewards = Reward(self.config_dict['state_dim'],
-                                self.config_dict['action_dim'],
-                                self.config_dict['hidden_dim'],
-                                self.config_dict['sequence_num'])
-        self.dones = Dones(self.config_dict['state_dim'],
-                            self.config_dict['action_dim'],
-                            self.config_dict['hidden_dim'],
-                            self.config_dict['sequence_num'])
-        self.dynamics_nn.load_state_dict(checkpoint['dynamics_state_dict'])
-        self.rewards_nn.load_state_dict(checkpoint['rewards_state_dict'])
-        self.dones_nn.load_state_dict(checkpoint['dones_state_dict'])
-
-    def reset(self, obs: ObsType) -> None:
-        self.states = obs.unsqueeze(0)
-        self.actions = torch.empty((0, self.config_dict['action_dim']))
-        self.rewards = torch.empty((0))
-        self.dones = torch.empty((0))
-        
-    def step(self, action: ActType) -> Tuple[ObsType, float, bool, float]:
-        self.actions = torch.cat((self.actions, action.unsqueeze(0)), dim=0)
-        steps = min(len(self.states), self.sequence_num)
-        states_ = self.states[-steps:].unsqueeze(0)
-        actions_ = self.actions[-steps:].unsqueeze(0)
-        states_actions = torch.cat([states_, actions_], dim=-1)
-        with torch.inference_mode():
-            next_state = self.dynamics_nn(states_actions)
-            reward = self.rewards_nn(states_actions)
-            done = self.dones_nn(states_actions)
-        self.states = torch.cat((self.states, next_state), dim=0)
-        self.rewards = torch.cat((self.rewards, reward), dim=0)
-        self.dones = torch.cat((self.dones, done), dim=0)
-        return next_state, reward, done>=self.config_dict["dones_cutoff"], done
-
-
 class Dynamics(nn.Module):
     def __init__(self, state_dim: int, action_dim: int, hidden_dim: int, num_layers: int):
         super().__init__()
         self.lstm = nn.LSTM(input_size=state_dim+action_dim, hidden_size=hidden_dim, num_layers=num_layers, batch_first=True)
         self.linear = nn.Linear(hidden_dim, state_dim)
-    def forward(self, x):
+    def forward(self, state, action):
+        x = torch.cat((state, action), dim=-1)
         x, _ = self.lstm(x)
         x = x[:, -1, :]
         x = self.linear(x)
@@ -77,7 +24,8 @@ class Reward(nn.Module):
         super().__init__()
         self.lstm = nn.LSTM(input_size=state_dim+action_dim, hidden_size=hidden_dim, num_layers=num_layers, batch_first=True)
         self.linear = nn.Linear(hidden_dim, 1)
-    def forward(self, x):
+    def forward(self, state, action):
+        x = torch.cat((state, action), dim=-1)
         x, _ = self.lstm(x)
         x = x[:, -1, :]
         x = self.linear(x)
@@ -88,15 +36,81 @@ class Dones(nn.Module):
         super().__init__()
         self.lstm = nn.LSTM(input_size=state_dim+action_dim, hidden_size=hidden_dim, num_layers=num_layers, batch_first=True)
         self.linear = nn.Linear(hidden_dim, 1)
-    def forward(self, x):
+    def forward(self, state, action):
+        x = torch.cat((state, action), dim=-1)
         x, _ = self.lstm(x)
         x = x[:, -1, :]
         x = self.linear(x)
         x = torch.sigmoid(x)
         return x.squeeze(-1)
 
+class Env:
+    states : Tensor
+    actions : Tensor
+    rewards : Tensor
+    dones : Tensor
+    config_dict : Dict[str, Any]
+    dynamics_nn: nn.Module
+    rewards_nn: nn.Module
+    dones_nn: nn.Module
+    state_dim: int
+    action_dim: int
+    hidden_dim: int
+    sequence_num: int
+    dones_cutoff: float
+
+    def __init__(self, chkpt_path: str = None):
+        self.load_from_chkpt(chkpt_path)
+
+    def load_from_chkpt(self, chk_path: str) -> None:
+        checkpoint = torch.load(chk_path)
+        self.config_dict = checkpoint['config']
+        self.state_dim = checkpoint['state_dim']
+        self.action_dim = checkpoint['action_dim']
+        self.hidden_dim = self.config_dict['hidden_dim']
+        self.sequence_num = self.config_dict['sequence_num']
+        self.dones_cutoff = self.config_dict['dones_cutoff']
+        self.dynamics_nn = Dynamics(self.state_dim, self.action_dim, self.hidden_dim, self.sequence_num)
+        self.rewards_nn = Reward(self.state_dim, self.action_dim, self.hidden_dim, self.sequence_num)
+        self.dones_nn = Dones(self.state_dim, self.action_dim, self.hidden_dim, self.sequence_num)
+        self.dynamics_nn.load_state_dict(checkpoint['dynamics'])
+        self.rewards_nn.load_state_dict(checkpoint['rewards'])
+        self.dones_nn.load_state_dict(checkpoint['dones'])
+
+    def reset(self, obs: ObsType) -> None:
+        self.states = obs
+        self.actions = torch.empty((0, self.action_dim))
+        self.rewards = torch.empty((0))
+        self.dones = torch.empty((0))
+        
+    def step(self, action: ActType) -> Tuple[ObsType, float, bool, float]:
+        self.actions = torch.cat((self.actions, action), dim=0)
+        steps = min(len(self.states), self.sequence_num)
+        states_ = self.states[-steps:].unsqueeze(0)
+        actions_ = self.actions[-steps:].unsqueeze(0)
+        with torch.inference_mode():
+            next_state = self.dynamics_nn(states_, actions_)
+            reward = self.rewards_nn(states_, actions_)
+            done = self.dones_nn(states_, actions_)
+        self.states = torch.cat((self.states, next_state), dim=0)
+        self.rewards = torch.cat((self.rewards, reward), dim=0)
+        self.dones = torch.cat((self.dones, done), dim=0)
+        return next_state, reward, done>=self.dones_cutoff, done
+
 def main():
-    pass
+    chkpt_path = "/home/james/sfu/ORL_optimizer/OtherModels/chkpt/halfcheetah_v3.pt"
+    env = Env(chkpt_path)
+    obs = torch.randn(1, 17)
+    print("init state", obs.numpy())
+    env.reset(obs)
+    for i in range(10):
+        action = torch.randn(1, 6)
+        next_state, reward, done, _ = env.step(action)
+        print("step", i+1)
+        print("action", action.numpy())
+        print("next_state", next_state.numpy())
+        print("reward", reward.numpy())
+        print("done", done.numpy())
 
 if __name__ == "__main__":
     main()
---
+++ OtherModels/env_util.py
@@ -0,0 +1,117 @@
+import uuid
+
+import gym
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from typing import TypeVar, List, Tuple, Dict, Any, Sequence, Union
+from dataclasses import asdict, dataclass
+import pyrallis
+from torch import Tensor
+from tqdm.auto import trange
+
+from env import Env, Dynamics, Reward, Dones, ObsType, ActType
+
+@dataclass
+class Config:
+    project: str = "ORL_optimizer"
+    group: str = "env"
+    name: str = "env_train"
+    env_name: str = "HalfCheetah-v3"
+    chkpt_path: str = "/home/james/sfu/ORL_optimizer/OtherModels/chkpt/halfcheetah_medium_v2.pt"
+    load_chkpt: bool = True    
+    save_chkpt_per: int = 1000
+    sequence_num: int = 5
+    hidden_dim: int = 256
+    dones_cutoff: float = 0.6
+    num_epochs: int = 10000
+    eval_episodes: int = 100
+    batch_size: int = 1024
+    dynamics_lr: float = 1e-3
+    rewards_lr: float = 1e-3
+    dones_lr: float = 1e-3
+    eval_seed: int = 0
+
+    def refresh_name(self):
+        self.name = f"{self.name}-{self.env_name}-{str(uuid.uuid4())[:8]}"
+
+
+def make_env(env_name: str, seed: int) -> gym.Env:
+    env = gym.make(env_name)
+    env.seed(seed)
+    env.action_space.seed(seed)
+    env.observation_space.seed(seed)
+    return env
+
+def get_env_info(env: gym.Env) -> Tuple[int, int]:
+    state_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.shape[0]
+    return state_dim, action_dim
+
+def sample_batch_online(
+    env: gym.Env, batch_size: int, sequence_num: int
+) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
+
+    seed = np.random.randint(0, 1e18)
+    env.seed(seed)
+    env.action_space.seed(seed)
+    env.observation_space.seed(seed)
+    samples_left = batch_size
+    state_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.shape[0]
+    states___ = torch.empty((0, sequence_num, state_dim))
+    actions___ = torch.empty((0, sequence_num, action_dim))
+    rewards___ = torch.empty((0, sequence_num))
+    dones___ = torch.empty((0, sequence_num))
+    next_states___ = torch.empty((0, sequence_num, state_dim))
+
+    while samples_left > 0:
+        obs = env.reset()
+        states = []
+        actions = []
+        rewards = []
+        dones = []
+        next_states = []
+        for _ in range(samples_left+sequence_num-1):
+            action = env.action_space.sample()
+            states.append(obs)
+            obs, reward, done, _ = env.step(action)
+            next_states.append(obs)
+            actions.append(action)
+            rewards.append(reward)
+            dones.append(done)
+            if done:
+                break
+        samples_cnt = len(states)-sequence_num+1
+        states = torch.tensor(np.array(states), dtype=torch.float32)
+        actions = torch.tensor(np.array(actions), dtype=torch.float32)
+        rewards = torch.tensor(np.array(rewards), dtype=torch.float32)
+        dones = torch.tensor(np.array(dones), dtype=torch.float32)
+        next_states = torch.tensor(np.array(next_states), dtype=torch.float32)
+        states_ = [states[i:i+samples_cnt] for i in range(sequence_num)]
+        states__ = torch.stack(states_, dim=1)
+        states___ = torch.cat((states___, states__), dim=0)
+        actions_ = [actions[i:i+samples_cnt] for i in range(sequence_num)]
+        actions__ = torch.stack(actions_, dim=1)
+        actions___ = torch.cat((actions___, actions__), dim=0)
+        rewards_ = [rewards[i:i+samples_cnt] for i in range(sequence_num)]
+        rewards__ = torch.stack(rewards_, dim=1)
+        rewards___ = torch.cat((rewards___, rewards__), dim=0)
+        dones_ = [dones[i:i+samples_cnt] for i in range(sequence_num)]
+        dones__ = torch.stack(dones_, dim=1)
+        dones___ = torch.cat((dones___, dones__), dim=0)
+        next_states_ = [next_states[i:i+samples_cnt] for i in range(sequence_num)]
+        next_states__ = torch.stack(next_states_, dim=1)
+        next_states___ = torch.cat((next_states___, next_states__), dim=0)
+        samples_left -= samples_cnt
+
+    batch = {
+        "states": states___,
+        "actions": actions___,
+        "rewards": rewards___,
+        "dones": dones___,
+        "next_states": next_states___,}
+    return batch
